e5a0223ea0537973b775f475ec56f894959937dc
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-09-17 15:38:48 +0200

    feat: fatto layout con le route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ListGames from './pages/ListGames'
+import DefaultLayout from './layout/DefaultLayout'
 
 function App() {
-  
+
 
   return (
     <>
-    <div className="container">
-      <h1>progetto finale react</h1>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout/>}>
+            <Route path='/' element={<ListGames/>}/>
+            <Route path='/products/:id'/>
+            <Route />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/ListGames.jsx
@@ -0,0 +1,7 @@
+export default function ListGames(){
+    return(
+        <>
+            <h1>Lista Games</h1>
+        </>
+    )
+}
