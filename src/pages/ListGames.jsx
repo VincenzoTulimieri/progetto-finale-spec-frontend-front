33adce055fab9aa154f70eed2ba3ea73976516ff
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-09-17 16:37:07 +0200

    feat: lista dei games in lista game

--- src/pages/ListGames.jsx
+++ src/pages/ListGames.jsx
@@ -1,4 +1,11 @@
+import { GlobalContext } from "../context/GlobalContext"
+import { useContext } from "react"
+import useGames from "../hook/useGames"
+
 export default function ListGames(){
+    const {games} = useContext(GlobalContext)
+    console.log(games)
+
     return(
         <>
             <h1>Lista Games</h1>
