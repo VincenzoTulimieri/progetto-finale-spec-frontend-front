5fec41003a28e027689cdacfe2b3a3e8061bb131
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-09-22 19:17:30 +0200

    feat: inserita wishlist in default layout

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom"
 import Header from "../components/Header"
+import { GlobalContext } from "../context/GlobalContext"
+import { useContext } from "react"
+import WishlistComponents from "../components/WishlistComponets"
 
 export default function DefaultLayout(){
+    const { games, open, removeItemWishlist, setOpen , wishlist } = useContext(GlobalContext)
     return(
         <>
             <Header/>
             <main className="container">
                     <Outlet />
+                    <WishlistComponents isOpen={open} setIsOpen={setOpen} wishlist={wishlist} game={games} removeItemWishlist={removeItemWishlist}/>
             </main>
         </>
     )
