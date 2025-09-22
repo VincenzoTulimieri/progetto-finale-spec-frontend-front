92406a0997ec661f0bbeca2d4a84731a50f92ad7
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-09-22 15:43:19 +0200

    fear: wishlist nel context con browser storege

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,12 +1,52 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import useGames from "../hook/useGames";
 
 export const GlobalContext = createContext()
 
 export function GlobalProvider({ children }) {
+    // products
     const { games } = useGames()
+    
+    // wishlist
+    const [open, setOpen] = useState(false)
+
+    const [wishlist, setWishlist] = useState(() => {
+        try {
+            const storedWishlist = localStorage.getItem('wishlist')
+            return storedWishlist ? JSON.parse(storedWishlist) : []
+
+        } catch (error) {
+            console.error('Dati non non recuperati', error)
+            return []
+        }
+    })
+
+    useEffect(() => {
+        try {
+            localStorage.setItem('wishlist', JSON.stringify(wishlist))
+        } catch (error) {
+            console.error('dati non salvati', error)
+        }
+
+    }, [wishlist])
+
+    // aggiungi alla wishlist
+    const addItemWishlist = (game) => {
+        setWishlist(prev => prev.find(g => g.id === game.id) ? prev : [...prev, game])
+    }
+
+    // rimuovi dalla wishlist
+    const removeItemWishlist = (id) => {
+        setWishlist(prev => prev.filter(g => g.id !== id))
+    }
+
+    // controllo se è presente in lista
+    const inWishlist = (id) => {
+        return wishlist.some(g => g.id === id)
+    }
+
     return (
-        <GlobalContext.Provider value={{games}}>
+        <GlobalContext.Provider value={{ games, open, wishlist, setOpen, addItemWishlist, removeItemWishlist, inWishlist }}>
             {children}
         </GlobalContext.Provider>
     )
