0ea0c7cf3dbd7ab70c5464a873e1fb6a5f5ab858
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-09-24 14:58:35 +0200

    feat: inserimento jumbotron

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,22 @@ import { GlobalContext } from "../context/GlobalContext"
 import { useContext } from "react"
 
 export default function Header() {
-    const {setOpen} = useContext(GlobalContext)
+    const { setOpen } = useContext(GlobalContext)
     return (
-        <header>
-            <nav className="navbar">
+        <header className="jumbotron">
+            <nav className="navbar vt-bg">
                 <div className="container-fluid justify-content-between">
                     <NavLink to={'/'} className='title-link navbar-brand navlink'>
-                        <img src="../public/logo-home2.png" alt="Logo" className="d-inline-block align-text-top logo-img" />
+                        <img src="/logo-home2.png" alt="Logo" className="d-inline-block align-text-top logo-img" />
                         <h1>Visual Gaming</h1>
                     </NavLink>
-                    <button className="btn-wishlist" onClick={()=>setOpen(true)}>wishlist</button>
+                    <button className="btn-wishlist" onClick={() => setOpen(true)}>wishlist</button>
                 </div>
             </nav>
+            <div className="jumbotron-content">
+                <h2>Benvenuto su Visual Gaming</h2>
+                <p>Scopri i migliori giochi da confrontare e aggiungere in wishlist</p>
+            </div>
         </header>
     )
 }
